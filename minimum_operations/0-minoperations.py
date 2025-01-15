d215e220c4a3e3526048a6897b1fd79033087748
audrey <m.audrey1@alustudent.com>
2025-01-15 15:38:18 +0200

    minimum operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
-"""Minimum Operations"""
+"""ALU interview questions."""
 
 
 def minOperations(n):
-        """Calculation  of operations needed to obtain exactly n H characters
-
-            Args:
-                    n (int): The target number of H characters.
-
-                        Returns:
-                                int: The minimum number of operations required.
-                                    """
-
-                                        if n <= 1:
+        """Minimum operations to reach n."""
+            if n <= 1:
+                        return 0
+                        for i in range(2, n + 1):
+                                    if n % i == 0:
+                                                    return minOperations(n // i) + i
                                                     return 0
-
-                                                    operations = 0
-                                                        i = 2
-                                                            while i <= n:
-                                                                        if n % i == 0:
-                                                                                        operations += i
-                                                                                                    n //= i
-                                                                                                            else:
-                                                                                                                            i += 1
-
-                                                                                                                                return operations
